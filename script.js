d34412060c73e0b2667d22a55a64c85a09be5233
Brad <brad.hankee@gmail.com>
2017-08-19 23:21:16 -0500

    Changed pictures and added animation to job cards.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 $('.carousel').carousel({
 	interval: 3000
 });
+
+$(window).scroll(function() {
+	var scroll = $(window).scrollTop();
+
+	//>=, not <=
+	if (scroll >= 400) {
+		//clearHeader, not clearheader - caps H
+		$('.centerCards').addClass('animated flipInY');
+	}
+}); //missing );
